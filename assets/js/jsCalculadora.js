250cec9d7299967dffc61462286257a208921e19
dyonatanlfs <dyonatan55@hotmail.com>
2022-06-09 16:25:06 -0300

    Inserção do front-end do projeto de uma calculadora proposta no bootcamp da DIO/TQI

---
+++ assets/js/jsCalculadora.js
@@ -0,0 +1,15 @@
+var valor1 = ;
+
+if (operacao == '*'){
+    multiplicacao ();
+} else if (operacao == '/') {
+    divisao ();
+} else if (operacao == '+') {
+    soma ();
+} else if (operacao == '-') {
+    subtracao ();
+} else if (operacao == '**') {
+    potenciacao ();
+} else if (operacao == 'PI') {
+    pi ();
+}
